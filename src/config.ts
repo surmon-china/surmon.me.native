7b97a2d08f3028550e7130b8332eff31010f12a0
surmon-china <surmon@foxmail.com>
2019-11-09 00:21:12 +0800

    Update gravatar api

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const appName = 'Surmon.me'
 export const webUrl = 'https://surmon.me'
 export const appApi = 'https://api.surmon.me'
 export const staticApi = 'https://cdn.surmon.me'
-export const gravatarApi = 'https://gravatar.surmon.me'
+export const gravatarApi = 'https://static.surmon.me/avatar'
 export const navigationPersistenceKey = __DEV__ ? '___NavigationStateDEV__' : null
 
 export const IS_DEV = __DEV__
